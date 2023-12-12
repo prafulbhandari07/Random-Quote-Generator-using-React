7150f82c58daceb848db9de615970ecb2669cb20
Praful Bhandari <128509560+prafulbhandari07@users.noreply.github.com>
2023-12-12 15:06:06 +0530

    React-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[quote, setQuote]=useState("Be yourself; everyone else is already taken.");
+  const[author, setAuthor]=useState("Anonymous");
+  async function getQuotes(){
+const response= await fetch("https://api.quotable.io/random");
+const data = await response.json();
+setQuote(data.content)
+setAuthor(data.author)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id="container">
+        <header>
+            <h1>Random Quote Generator</h1>
+        </header>
+        <div class="quote-container">
+            <div class="quote-box">
+                <p id="quote">"{quote}”</p>
+                <small id="author">-{author}</small>
+            </div>
+            <button onClick={getQuotes} id="button">Generate</button>
+        </div>
     </div>
   );
 }
